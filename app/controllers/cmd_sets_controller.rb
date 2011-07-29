d4d0eece8db4eb485a074508b9e30f9c2b83d768
fsword <li.jianye@gmail.com>
2011-07-29 21:54:10 +0800

    创建命令包时提示信息

--- app/controllers/cmd_sets_controller.rb
+++ app/controllers/cmd_sets_controller.rb
@@ -5,7 +5,7 @@ class CmdSetsController < BaseController
   
   def create
     cmd_set = current_app.cmd_set_defs.find(params[:cmd_set_def_id]).create_cmd_set(current_user)
-    render :text => cmd_set.to_json
+    render :text => "命令包已创建" #cmd_set.to_json
   end
   
   def show
