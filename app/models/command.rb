c1a03d72e187051357bf105d78adc250055f77ec
fsword <li.jianye@gmail.com>
2011-08-02 10:24:18 +0800

    state_machine的状态使用中文（未来会使用 i18n 的方式）

--- app/models/command.rb
+++ app/models/command.rb
@@ -30,12 +30,12 @@ class Command < ActiveRecord::Base
     self
   end
   
-  state_machine :state, :initial => :ready do
-    event :invoke do transition :ready => :running end
-    event :failure do transition :running => :fail end
-    event :acknowledge do transition :fail => :done end
-    event :ack do transition :fail => :done end
-    event :success do transition :running => :done end
+  state_machine :state, :initial => '已创建' do
+    event :invoke do transition '已创建' => '执行中' end
+    event :failure do transition '执行中' => '执行失败' end
+    event :acknowledge do transition '执行失败' => '结束' end
+    event :ack do transition '执行失败' => '结束' end
+    event :success do transition '执行中' => '结束' end
   end
   
 end
