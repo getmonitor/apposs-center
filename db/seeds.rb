eeed8f5ae1040006a3c3d6cd527c975191e7e046
li.jianye@gmail.com <li.jianye@gmail.com@e3cf8a15-5072-d663-4c44-a26c1ac8da71>
2011-08-21 10:41:10 +0000

    精简初始化数据
    git-svn-id: https://apposs-center.googlecode.com/svn/trunk@18 e3cf8a15-5072-d663-4c44-a26c1ac8da71

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,33 @@
 # cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 # Mayor.create(:name => 'Daley', :city => cities.first)
 
-cmd_group = CmdGroup.create :name => 'jboss'
+#cmd_group = CmdGroup.create :name => 'jboss'
 
-cmd_group2 = CmdGroup.create :name => 'nodejs'
+#cmd_group2 = CmdGroup.create :name => 'nodejs'
 
-cmd_group.cmd_defs << CmdDef.create(:name => 'echo start')
-cmd_group.cmd_defs << CmdDef.create(:name => 'echo stop')
-cmd_group.cmd_defs << CmdDef.create(:name => 'echo redeploy')
+#cmd_group.cmd_defs << CmdDef.create(:name => 'echo start')
+#cmd_group.cmd_defs << CmdDef.create(:name => 'echo stop')
+#cmd_group.cmd_defs << CmdDef.create(:name => 'echo redeploy')
 
-cmd_group2.cmd_defs << CmdDef.create( :name => 'echo start_nodejs')
-cmd_group2.cmd_defs << CmdDef.create( :name => 'echo stop_nodejs')
+#cmd_group2.cmd_defs << CmdDef.create( :name => 'echo start_nodejs')
+#cmd_group2.cmd_defs << CmdDef.create( :name => 'echo stop_nodejs')
 
-app = App.create(:name => 'sample-app',:online => true)
-app.machines << Machine.create(:name => 'tanx1.cnz',:host => 'localhost',:port => 22)
-app.machines << Machine.create(:name => 'tanx2.cnz',:host => 'test',:port => 22)
-Machine.create(:name => 'tanx3.cnz',:host => 'test',:port => 22)
+#app = App.create(:name => 'sample-app',:online => true)
+#app.machines << Machine.create(:name => 'tanx1.cnz',:host => 'localhost',:port => 22)
+#app.machines << Machine.create(:name => 'tanx2.cnz',:host => 'test',:port => 22)
+#Machine.create(:name => 'tanx3.cnz',:host => 'test',:port => 22)
 
-app.cmd_set_defs << CmdSetDef.create(:name => "upgrade package", :expression => '1,2|true,4')
+#app.cmd_set_defs << CmdSetDef.create(:name => "upgrade package", :expression => '1,2|true,4')
 #
 
 
-pe_role = Role.create(:name => Role::PE)
 admin_role = Role.create(:name => Role::Admin)
+pe_role = Role.create(:name => Role::PE)
 appops_role = Role.create(:name => Role::APPOPS)
 u = User.create(:email => 'lifu@taobao.com', :password => 'hahaha')
-Stakeholder.create :user => u, :role => admin_role, :app => app
+Stakeholder.create :user => u, :role => admin_role
 
-%w{cnz cm3 cm4}.each{|name| Room.create(:name => name)}
-Room.first.machines << Machine.all
+#%w{cnz cm3 cm4}.each{|name| Room.create(:name => name)}
+#Room.first.machines << Machine.all
 
-App.first.cmd_set_defs.first.create_cmd_set User.first
+#App.first.cmd_set_defs.first.create_cmd_set User.first
