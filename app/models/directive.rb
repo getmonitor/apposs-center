ff2eb8ab4dde0101fadd84aa15e333013905f352
li.jianye@gmail.com <li.jianye@gmail.com@e3cf8a15-5072-d663-4c44-a26c1ac8da71>
2011-08-22 13:24:43 +0000

    状态更新联动
    git-svn-id: https://apposs-center.googlecode.com/svn/trunk@27 e3cf8a15-5072-d663-4c44-a26c1ac8da71

--- app/models/directive.rb
+++ app/models/directive.rb
@@ -9,8 +9,7 @@ class Directive < ActiveRecord::Base
     self.isok = isok
     self.response = body
     isok ? ok : error
-    #TODO 更新 operation state
-    operation.done
+    
   end
 
   state_machine :state, :initial => :init do
@@ -19,6 +18,24 @@ class Directive < ActiveRecord::Base
     event :error do transition :running => :failure end
     event :ok do transition :running => :done end
     event :ack do transition :failure => :done end
+
+    after_transition :on => :invoke, :do => :fire_operation
+    after_transition :on => :error, :do => :error_operation
+    after_transition :on => :ok, :do => :check_ok_operation
   end
 
+  def fire_operation
+    operation.fire
+  end
+  
+  def error_operation
+    operation.error
+  end
+  
+  def check_ok_operation
+    if operation.directives.without_state(:done).count == 0
+      operation.ok
+    end
+  end
+  
 end
