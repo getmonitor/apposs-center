e20372954e82e535cf4514a673f8d97db423ac59
fsword <li.jianye@gmail.com>
2011-09-05 14:12:18 +0800

    bug fix: 文件编码修改

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -28,6 +28,7 @@ class OperationsController < BaseController
       choosed_machine_ids = params[:choosedMachines].split(',').collect { |s| s.to_i }.uniq
     end
     current_app.operation_templates.find(params[:operation_template_id]).gen_operation(current_user, choosed_machine_ids)
+    render :text => "操作已创建"
   end
 
   def continue
