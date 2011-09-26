ee98d718dfed6be81bb9da186321404d7fc1a0f0
fsword <li.jianye@gmail.com>
2011-09-26 17:32:00 +0800

    补充 pair 的测试用例

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -2,9 +2,14 @@
 require 'spec_helper'
 
 describe Property do
-  fixtures :properties,:apps,:rooms,:machines,:envs
+  fixtures :properties
   it "支持 global property" do
     Property.global.where( :name => 'root').first.value.should == '/home/a'
     Property.global.where( :name => 'conf_file').first.value.should == '/home/a/conf/pe.conf'
+  end
+  
+  it "支持输出 hash" do
+    Property.global.pairs['root'].should == '/home/a'
+    Property.global.pairs['conf_file'].should == '/home/a/conf/pe.conf'
   end
 end
