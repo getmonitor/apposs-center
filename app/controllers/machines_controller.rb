d96f225eabe731b8b49850e589c26a764448ed42
fsword <li.jianye@gmail.com>
2011-10-01 03:03:23 +0800

    支持机器列表和操作模板列表的刷新

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,22 +1,22 @@
-class MachinesController < BaseController
+class MachinesController < ResourceController
 
-  def index
-    machines = current_app.machines.paginate(:per_page => params[:limit].to_i, :page => params[:page].to_i)
-    total = current_app.machines.count
+#  def index
+#    machines = current_app.machines.paginate(:per_page => params[:limit].to_i, :page => params[:page].to_i)
+#    total = current_app.machines.count
 
-    respond_with :totalCount => total, :machines => machines
-  end
+#    respond_with :totalCount => total, :machines => machines
+#  end
 
-  def command_state
-    directives = current_app.machines.find(params[:id]).directives.normal.collect do |o|
-      o.attributes.update(
-          "leaf" => true,
-          "state" => o.human_state_name,
-          "name" => o.command_name
-      )
-    end
-    respond_with directives
-  end
+#  def command_state
+#    directives = current_app.machines.find(params[:id]).directives.normal.collect do |o|
+#      o.attributes.update(
+#          "leaf" => true,
+#          "state" => o.human_state_name,
+#          "name" => o.command_name
+#      )
+#    end
+#    respond_with directives
+#  end
 
   def reset
     respond_with current_app.machines.find(params[:id]).resume
