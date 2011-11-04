5360f8b563f32b6071bedaf1ce5c31c3d3f6bd01
fsword <li.jianye@gmail.com>
2011-11-04 20:38:46 +0800

    修正登录机制，调整相关界面

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,16 @@ Apposs::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.default_url_options = { :host => 'localhost:9999' }  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'localhost:9999',
+    :user_name            => 'root.apposs',
+    :password             => '1234qwerasdf!QAZ@WSX',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
 
