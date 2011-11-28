cc63cfda6f46f16749302386851ce3138085f01f
fsword <li.jianye@gmail.com>
2011-11-28 20:33:08 +0800

    临时更新

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,15 @@ describe User do
     
     u.is_admin?.should be_false
     # 返回值表示保存成功
-    u.grant(Role::Admin,nil).new_record?.should == false
+    u.grant(Role::Admin,nil).new_record?.should be_false
     # 重复赋权会导致保存失败
-    u.grant(Role::Admin,nil).new_record?.should == true
+    u.grant(Role::Admin,nil).new_record?.should be_true
     u.is_admin?.should be_true
 
     u.is_pe?(App.find(1)).should be_true
     u.ungrant(Role::PE, App.find(1)).should_not be_nil
     u.is_pe?(App.find(1)).should be_false
-    u.grant(Role::PE, App.find(1)).new_record?.should == false
+    u.grant(Role::PE, App.find(1)).new_record?.should be_false
     u.is_pe?(App.find(1)).should be_true
 
     u.is_appops?(App.find(1)).should be_true
