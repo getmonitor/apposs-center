8e852d73431d7cdef3c5ab306ee3164a47a1bdc6
fsword <li.jianye@gmail.com>
2012-01-08 00:27:16 +0800

    keyword功能增强
    * 添加words方法，简化操作
    * 支持tags

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -3,8 +3,31 @@ require 'spec_helper'
 
 describe Keyword do
   
-  it '支持关键字子类' do
-    keyword = Dangerous.create :value => 'rm'
-    keyword.type.should == 'Dangerous'
+  describe '创建' do
+    it '支持 Dangerous 子类创建' do
+      dangerous = Dangerous.create :value => 'rm'
+      dangerous.type.should == 'Dangerous'
+      Keyword.subclasses.should include(Dangerous)
+    end
+    
+    it '不支持本身创建' do
+      Keyword.new(:value => 'mv').valid?.should be_false
+    end
+  end
+  
+  describe '访问、修改' do
+    fixtures :keywords
+    it '支持简洁的访问api' do
+      Dangerous.words.count.should == 2
+      Dangerous.words.should include('mv')
+      Keyword.words.count.should == 3
+    end
+    
+    it '支持标签访问' do
+      danger = Dangerous.first
+      danger.tag_list = %w{shell,delete}
+      danger.save!
+      danger.tags.count.should == 2
+    end
   end
 end
